8c1b63c3cc5a9b042a4f3de2b26eb1dbadf0b05f
Rodrigo <rodrigogalvann1@gmail.com>
2022-09-21 21:08:23 -0300

    crea primeros archivos y primera funcionalidad

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const $startGame = document.querySelector('#start-game')
+const $introGame = document.querySelector('.intro-game')
+
+const startGame = () =>{
+
+    $introGame.className = 'oculto'
+
+}
+
+$startGame.onclick = startGame
