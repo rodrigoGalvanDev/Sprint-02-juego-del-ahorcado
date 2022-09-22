067d462f6a2234750b96a389b5285df1c531ba78
Rodrigo <rodrigogalvann1@gmail.com>
2022-09-22 10:44:08 -0300

    agrega funcionalidades a los botones

--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
-const $startGame = document.querySelector('#start-game')
-const $introGame = document.querySelector('.intro-game')
+const $startGameButton = document.querySelector("#start-game")
+const $introGame = document.querySelector("#intro-game")
+const $addWordButton = document.querySelector("#add-word")
+const $addWordGame = document.querySelector("#add-word-game")
+const $cancellGameButton = document.querySelector('#cancell-game')
+
+
 
 const startGame = () =>{
 
-    $introGame.className = 'oculto'
+    $introGame.style.display = 'none'
+
+}
+ 
+
+const addWord = () =>{
+
+    $introGame.style.display = 'none'
+    $addWordGame.style.display = 'block'
 
 }
 
-$startGame.onclick = startGame
+const cancellGame = () =>{
+
+    $addWordGame.style.display = 'none'
+    $introGame.style.display = 'flex'
+}
+
+
+
+$startGameButton.onclick = startGame
+
+$addWordButton.onclick = addWord
+$cancellGameButton.onclick = cancellGame
